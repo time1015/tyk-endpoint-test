ad448fa452fa5417a2dd015cda257e23ae8aef8a
John Daniel Regino <time1015@users.noreply.github.com>
2019-12-04 11:15:39 +0800

    Remove report-on-catch on main

--- client/src/main/java/io/se7en/apigwtest/Main.java
+++ client/src/main/java/io/se7en/apigwtest/Main.java
@@ -1,8 +1,5 @@
 package io.se7en.apigwtest;
 
-import javax.net.ssl.SSLException;
-import javax.ws.rs.ProcessingException;
-
 public class Main {
   public static void main(String[] arguments) throws Throwable {
     MainConfiguration config = new MainConfiguration(arguments);
@@ -23,14 +20,6 @@ public class Main {
   private static void doHttpsTest(MainConfiguration config) throws Throwable {
     try (PingTest test = new PingTest()) {
       test.execute(config.httpsBasePathGenerator());
-    } catch (ProcessingException e) {
-      Throwable cause = e.getCause();
-
-      if (cause instanceof SSLException) {
-        System.out.println("Unable to establish HTTPS conneciton; the server might not support it.");
-      } else {
-        throw cause;
-      }
     }
   }
 }
